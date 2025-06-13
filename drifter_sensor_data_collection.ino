4c2d041f03dd9be89357e238c237bb79c1ee5bfd
ronBonVoN <veronika.l.davis@gmail.com>
2025-06-13 08:43:01 -0400

    work done 6/12/25.  promising run time
    the code has been running on the arduino since 11:15am on 6/12 without freezing. i am writing this at 8:30am 6/13.
    1) i made it so i2c and spi turn off and turn of with the relay.
    2) i've seen the watchdog work to restart the arduino a few times.
    3) i made it so oled reset and sd chip select are not on the same pin.
    i think the compination of these changes has allowed the program to run longer.

--- drifter_sensor_data_collection.ino
+++ drifter_sensor_data_collection.ino
@@ -1,3 +1,4 @@
+//https://www.amazon.com/dp/B0B1ZHXXXD?linkCode=sl1&tag=zlufy-20&linkId=8b8069b6630f617304884d920146598f&language=en_US&ref_=as_li_ss_tl&th=1
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
@@ -14,25 +15,25 @@
 #include <avr/interrupt.h>
 
 #define I2C_ADDRESS (0x67)
+#define SCREEN_ADDRESS (0x3C)
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET     4
-#define SCREEN_ADDRESS (0x3C)
+#define RELAY_PIN 7 
+#define SDCHIP_SELECT 53
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_MCP9600 mcp;
 TinyGPSPlus gps;
 
-const int chipSelect = 53;
 int runs = 0; 
 int m = -1, d = -1, y = -1, hr = -1, min = -1, sec = -1; 
-//String date = "-1", time = "-1"; 
 float lat = -1.0, lng = -1.0, speed = -1.0, heading = -1.0, tempHot = -1.0, tempCold = -1.0, turbidity = -1.0, oxygen = -1.0; 
 const char fileName[10] = "testing8.txt"; 
 char line[150];
 char data[14][10];
-volatile bool shouldWake = false;
 bool serialEnable = 0; 
+volatile bool shouldWake = false;
 
 /*ISR(WDT_vect) {
   shouldWake = true;
@@ -44,13 +45,13 @@ void setup() {
   if (serialEnable) Serial.begin(9600);
   Serial1.begin(9600);
   Wire.begin(); 
-  pinMode(4, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
   delay(2000); 
-  digitalWrite(4, HIGH); 
+  digitalWrite(RELAY_PIN, HIGH); 
   delay(2000); 
 
   initialize(display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS), "display");
-  initialize(SD.begin(chipSelect), "SD card");
+  initialize(SD.begin(SDCHIP_SELECT), "SD card");
   initialize(mcp.begin(I2C_ADDRESS), "thermocouple");
 
   File dataFile = SD.open(fileName, FILE_WRITE);
@@ -68,8 +69,8 @@ void loop() {
   if (runs<2) {
     runs++;  
     
-    pinMode(4, OUTPUT);
-    SD.begin(chipSelect);   
+    pinMode(RELAY_PIN, OUTPUT);
+    SD.begin(SDCHIP_SELECT);   
       
     unsigned long start = millis();
     do{
@@ -79,24 +80,90 @@ void loop() {
 
     m = gps.date.month(); d = gps.date.day(); y = gps.date.year(); 
     hr = gps.time.hour(); min = gps.time.minute(); sec = gps.time.second(); 
+    lat = gps.location.lat(); lng = gps.location.lng(); 
+    speed = gps.speed.mps(); heading = gps.course.deg(); 
+    tempHot = mcp.readThermocouple(); tempCold = mcp.readAmbient();
+    turbidity = -1174.7 * analogRead(A0) * (5.0/1024.0) + 5049.1;
+    oxygen = analogRead(A1) * (5.0/1024.0);
+
+    snprintf(data[0], 3, "%02d", m);
+    snprintf(data[1], 3, "%02d", d);
+    snprintf(data[2], 5, "%04d", y);
+    snprintf(data[3], 3, "%02d", hr);
+    snprintf(data[4], 3, "%02d", min);
+    snprintf(data[5], 3, "%02d", sec);
+    dtostrf(lat,       6, 2, data[6]);
+    dtostrf(lng,       6, 2, data[7]);
+    dtostrf(speed,     5, 2, data[8]);
+    dtostrf(heading,   6, 2, data[9]);
+    dtostrf(tempHot,   6, 2, data[10]); 
+    dtostrf(tempCold,  6, 2, data[11]); 
+    dtostrf(turbidity, 7, 2, data[12]); 
+    dtostrf(oxygen,    5, 2, data[13]);
+  
+    snprintf(line, sizeof(line), "%s-%s-%s\t%s:%s:%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
+      data[0], data[1], data[2], data[3], data[4], data[5],
+      data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13]);
+    File dataFile = SD.open(fileName, FILE_WRITE);
+    println_SerialFile(line, dataFile); 
+    /*
+    println_SerialFile(
       String(m)+"-"+String(d)+"-"+String(y)+"\t"+
       String(hr)+":"+String(min)+":"+String(sec)+"\t"+
+      time+"\t"+
+      String(lat)+"\t"+
+      String(lng)+"\t"+
+      String(speed)+"\t"+
+      String(heading)+"\t"+
+      String(tempHot)+"\t"+
+      String(tempCold) +"\t"+
+      String(turbidity)+"\t"+
+      String(oxygen), dataFile); 
+*/
+    display.clearDisplay(); 
+    display.setTextColor(WHITE); 
+    display.setTextSize(1); 
+    snprintf(line, sizeof(line), "%s-%s-%s %s:%s:%s", data[0], data[1], data[2], data[3], data[4], data[5]);
+    display.setCursor(0,0);  display.print(line); 
+    snprintf(line, sizeof(line), "lat:%s lng:%s", data[6], data[7]);
+    display.setCursor(0,10); display.print(line); 
+    snprintf(line, sizeof(line), "mph:%s deg:%s", data[8], data[9]);
+    display.setCursor(0,20); display.print(line);
+    snprintf(line, sizeof(line), "temp:%s, %s", data[10], data[11]);
+    display.setCursor(0,30); display.print(line);
+    snprintf(line, sizeof(line), "turbidity: %s", data[12]);
+    display.setCursor(0,40); display.print(line);
+    snprintf(line, sizeof(line), "oxygen:    %s", data[13]);
+    display.setCursor(0,50); display.print(line);
+    display.display(); 
+  }
+
   else {
   runs = 0; 
-  wdt_enable(WDTO_8S); 
-  print_SerialDisplay("Entering sleep mode...", 0); 
-  wdt_reset(); 
+  wdt_enable(WDTO_8S);             
   
-  digitalWrite(4, LOW); 
-  delay(5000);  //sleep_timed(16);
-  digitalWrite(4,HIGH);
-  
+  print_SerialDisplay("Entering sleep mode...", 0); //might need to move this to after turning off relay
   wdt_reset(); 
-  delay(3000); 
+
+  power_spi_disable();
+  power_twi_disable();
+  wdt_reset(); 
+  delay(2000); 
+
+  digitalWrite(RELAY_PIN, LOW); 
+  delay(5000);                      // sleep_timed(16);
+  digitalWrite(RELAY_PIN, HIGH);
+  wdt_reset(); 
+
+  power_spi_enable();
+  power_twi_enable();
+  wdt_reset(); 
+  delay(2000); 
+  
   print_SerialDisplay("out of sleep mode.", 1); 
   wdt_disable();
   delay(3000); 
-  }
+}
 
 }
 
@@ -191,3 +258,5 @@ void sleep_timed(int secs) {
     delay(50);         // Short delay to avoid CPU hammering
   }
 }*/
+
+
