4e4fecd9aab72fe4c16b3136382ef499ee468cc4
ronBonVoN <veronika.l.davis@gmail.com>
2025-06-23 15:11:53 -0400

    simplifying file writing
    last program ran for at least 72hrs, but i do not know how long it actually ran because no file was created to write to. i suspect i used a bad file name. now i will try again with this program, but i'm starting with around 50% battery power instead of 100%.
    - changed file name.
    - made it so 1 data point is collected every 10mins instead of 2.
    - got rid of the function that was supposed to alert when battery is about to die. seemed to be too sensitive to be trustable.
    - tried to simplify and optimize my writing to serial, display, and file functions.

--- drifter_sensor_data_collection.ino
+++ drifter_sensor_data_collection.ino
@@ -1,5 +1,9 @@
-//https://www.amazon.com/dp/B0B1ZHXXXD?linkCode=sl1&tag=zlufy-20&linkId=8b8069b6630f617304884d920146598f&language=en_US&ref_=as_li_ss_tl&th=1
-#include <SPI.h>
+/*to-do
+try putting scl and sda on same lines 
+test turning off display
+update github 
+test file*/
+
 #include <SD.h>
 #include <Wire.h>
 #include <Adafruit_I2CDevice.h>
@@ -25,14 +29,12 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_MCP9600 mcp;
 TinyGPSPlus gps;
-
-int runs = 0; 
+File dataFile; 
 int m = -1, d = -1, y = -1, hr = -1, min = -1, sec = -1; 
 float lat = -1.0, lng = -1.0, speed = -1.0, heading = -1.0, tempHot = -1.0, tempCold = -1.0, turbidity = -1.0, oxygen = -1.0; 
-const char fileName[10] = "9:30am_6.17.txt"; 
-char line[150];
-char data[14][10];
-bool serialEnable = 0; 
+const char fileName[10] = "T623.txt"; 
+char line[200], data[14][10];
+const bool serialEnable = 0; 
 //volatile bool shouldWake = false;
 
 /*ISR(WDT_vect) {
@@ -40,7 +42,6 @@ bool serialEnable = 0;
 }*/
 
 void setup() {
-
   delay(10000); 
   if (serialEnable) Serial.begin(9600);
   Serial1.begin(9600);
@@ -55,74 +56,47 @@ void setup() {
   initialize(SD.begin(SDCHIP_SELECT), "SD card");
   initialize(mcp.begin(I2C_ADDRESS), "thermocouple");
 
-  File dataFile = SD.open(fileName, FILE_WRITE);
-  if (dataFile) {
-    println_SerialFile("Date\tTime\tLatitude\tLongitude\tSpeed\tHeading\tHot Junction\tCold Junction\tTurbidity\tOxygen", dataFile);
-  }
-  else {
-    snprintf(line, sizeof(line), "error opening %s", fileName);
-    print_SerialDisplay(line, 1);
-    while(1); 
-  }
+  print_SerialFile("Date\tTime\tLatitude\tLongitude\tSpeed\tHeading\tHot Junction\tCold Junction\tTurbidity\tOxygen\n....\tUTC time\t....\t....\tmph\tdeg\tC\tC\tntu\t....\n");
 }
 
 void loop() {
-  if (runs<1) {
-    runs++;  
+  pinMode(RELAY_PIN, OUTPUT);
     
-    pinMode(RELAY_PIN, OUTPUT);
-    SD.begin(SDCHIP_SELECT); 
-    File dataFile = SD.open(fileName, FILE_WRITE);
-    
-    if (readVcc()<4700) println_SerialFile("***low battery***", dataFile); 
-      
-    unsigned long start = millis();
-    do{
-        while (Serial1.available())
-        gps.encode(Serial1.read()); 
-    } while (millis()-start < 360000);//3mins 
+  unsigned long start = millis();
+  do{
+      while (Serial1.available())
+      gps.encode(Serial1.read()); 
+  } while (millis()-start < 180000);
 
-    m = gps.date.month(); d = gps.date.day(); y = gps.date.year(); 
-    hr = gps.time.hour(); min = gps.time.minute(); sec = gps.time.second(); 
-    lat = gps.location.lat(); lng = gps.location.lng(); 
-    speed = gps.speed.mps(); heading = gps.course.deg(); 
-    tempHot = mcp.readThermocouple(); tempCold = mcp.readAmbient();
-    turbidity = -1174.7 * analogRead(A0) * (5.0/1024.0) + 5049.1;
-    oxygen = analogRead(A1) * (5.0/1024.0);
+  m = gps.date.month(); d = gps.date.day(); y = gps.date.year(); 
+  hr = gps.time.hour(); min = gps.time.minute(); sec = gps.time.second(); 
+  lat = gps.location.lat(); lng = gps.location.lng(); 
+  speed = gps.speed.mps(); heading = gps.course.deg(); 
+  tempHot = mcp.readThermocouple(); tempCold = mcp.readAmbient();
+  turbidity = -1174.7 * analogRead(A0) * (5.0/1024.0) + 5049.1;
+  oxygen = analogRead(A1) * (5.0/1024.0);
 
-    snprintf(data[0], 3, "%02d", m);
-    snprintf(data[1], 3, "%02d", d);
-    snprintf(data[2], 5, "%04d", y);
-    snprintf(data[3], 3, "%02d", hr);
-    snprintf(data[4], 3, "%02d", min);
-    snprintf(data[5], 3, "%02d", sec);
-    dtostrf(lat,       6, 2, data[6]);
-    dtostrf(lng,       6, 2, data[7]);
-    dtostrf(speed,     5, 2, data[8]);
-    dtostrf(heading,   6, 2, data[9]);
-    dtostrf(tempHot,   6, 2, data[10]); 
-    dtostrf(tempCold,  6, 2, data[11]); 
-    dtostrf(turbidity, 7, 2, data[12]); 
-    dtostrf(oxygen,    5, 2, data[13]);
-  
-    snprintf(line, sizeof(line), "%s-%s-%s\t%s:%s:%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s",
-      data[0], data[1], data[2], data[3], data[4], data[5],
-      data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13]);
-    println_SerialFile(line, dataFile); 
-    /*
-    println_SerialFile(
-      String(m)+"-"+String(d)+"-"+String(y)+"\t"+
-      String(hr)+":"+String(min)+":"+String(sec)+"\t"+
-      time+"\t"+
-      String(lat)+"\t"+
-      String(lng)+"\t"+
-      String(speed)+"\t"+
-      String(heading)+"\t"+
-      String(tempHot)+"\t"+
-      String(tempCold) +"\t"+
-      String(turbidity)+"\t"+
-      String(oxygen), dataFile); 
-*/
+  snprintf(data[0],  3, "%02d", m);
+  snprintf(data[1],  3, "%02d", d);
+  snprintf(data[2],  5, "%04d", y);
+  snprintf(data[3],  3, "%02d", hr);
+  snprintf(data[4],  3, "%02d", min);
+  snprintf(data[5],  3, "%02d", sec);
+  dtostrf(lat,       6, 2, data[6]);
+  dtostrf(lng,       6, 2, data[7]);
+  dtostrf(speed,     5, 2, data[8]);
+  dtostrf(heading,   6, 2, data[9]);
+  dtostrf(tempHot,   6, 2, data[10]); 
+  dtostrf(tempCold,  6, 2, data[11]); 
+  dtostrf(turbidity, 7, 2, data[12]); 
+  dtostrf(oxygen,    5, 2, data[13]);
+
+  snprintf(line, sizeof(line), "%s-%s-%s\t%s:%s:%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+    data[0], data[1], data[2], data[3], data[4], data[5],
+    data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13]);
+  print_SerialFile(line); 
+
+  if (displayPresent()) {
     display.clearDisplay(); 
     display.setTextColor(WHITE); 
     display.setTextSize(1); 
@@ -139,81 +113,73 @@ void loop() {
     snprintf(line, sizeof(line), "oxygen:    %s", data[13]);
     display.setCursor(0,50); display.print(line);
     display.display(); 
+    delay(5000); 
   }
-
-  else {
-  runs = 0; 
+  /******************************** turning relay off ****************************************/
   wdt_enable(WDTO_8S);             
   
-  print_SerialDisplay("Entering sleep mode...", 0); //might need to move this to after turning off relay
-  wdt_reset(); 
-
-  power_spi_disable();
-  power_twi_disable();
-  wdt_reset(); 
-  delay(2000); 
+  print_SerialDisplay("Entering sleep mode..."); wdt_reset(); 
+  power_spi_disable(); power_twi_disable(); wdt_reset(); delay(2000); 
 
   digitalWrite(RELAY_PIN, LOW); 
-  for(int i=0; i<120; i++) { //10min wait
-  wdt_reset(); 
-  delay(5000);
+  for(int i=0; i<75; i++) { 
+    wdt_reset(); 
+    delay(8000); 
+    wdt_reset(); 
   }
-  digitalWrite(RELAY_PIN, HIGH);
+  digitalWrite(RELAY_PIN, HIGH); 
   wdt_reset(); 
 
-  power_spi_enable();
-  power_twi_enable();
-  wdt_reset(); 
-  delay(2000); 
-  
-  print_SerialDisplay("out of sleep mode.", 1); 
-  wdt_disable();
-  delay(3000); 
-}
-
+  power_spi_enable(); power_twi_enable(); wdt_reset(); delay(2000); 
+  print_SerialDisplay("out of sleep mode.\n"); wdt_disable(); delay(3000); 
 }
 
 void initialize(bool x, const char* sensor) {
   snprintf(line, sizeof(line), "Initializing %s", sensor, "...");
-  print_SerialDisplay(line, 0);
+  print_SerialDisplay(line);
   if(!x) {
-    snprintf(line, sizeof(line), "%s failed", sensor);
-    print_SerialDisplay(line, 1);
-    while (1); 
+    snprintf(line, sizeof(line), "%s failed\n", sensor);
+    print_SerialDisplay(line);
+    delay(180000); 
   }
-  snprintf(line, sizeof(line), "%s initialization done.", sensor);
-  print_SerialDisplay(line, 1);
-}
-
-void print_SerialDisplay(const char* message, bool end) {
-  if (serialEnable) {
-  if (end) Serial.println(message); 
-  else Serial.print(message); 
+  else {
+    snprintf(line, sizeof(line), "%s initialization done.\n", sensor);
+    print_SerialDisplay(line);
   }
-  display.clearDisplay(); 
-  display.setTextColor(WHITE); 
-  display.setTextSize(1); 
-  display.setCursor(0,20);
-  display.print(message); 
-  display.display();
 }
 
-/*void print_SerialDisplay(String s, bool end) {
-  if (end) Serial.println(s); 
-  else Serial.print(s); 
-  display.clearDisplay(); 
-  display.setTextColor(WHITE); 
-  display.setTextSize(1); 
-  display.setCursor(0,20);
-  display.print(s); 
-  display.display();
-}*/
-
-void println_SerialFile(const char* message, File dataFile) {
-  if (serialEnable) Serial.println(message); 
-  dataFile.println(message);
-  dataFile.close(); 
+bool displayPresent() {
+  Wire.beginTransmission(SCREEN_ADDRESS);
+  return (Wire.endTransmission() == 0);
 }
+
+void print_SerialDisplay(const char* message) {
+  if (serialEnable) Serial.print(message); 
+  if (displayPresent()) {
+    display.clearDisplay(); 
+    display.setTextColor(WHITE); 
+    display.setTextSize(1); 
+    display.setCursor(0,20);
+    display.print(message); 
+    display.display();
+  }
+}
+
+void print_SerialFile(const char* message) {
+  SD.begin(SDCHIP_SELECT); 
+  if (serialEnable) Serial.print(message); 
+  dataFile = SD.open(fileName, FILE_WRITE);
+  if (dataFile) {
+   dataFile.print(message);
+   dataFile.close(); 
+  }
+  else {
+    snprintf(line, sizeof(line), "Error opening %s\n", fileName);
+    print_SerialDisplay(line);
+    delay(120000); 
+  }
+}
+
 /*
 void sleep_timed(int secs) {
   int loops = secs/8; 
@@ -257,14 +223,6 @@ void sleep_timed(int secs) {
   }
 }*/
 
-long readVcc() {
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1); 
-  delay(2); 
-  ADCSRA |= _BV(ADSC); 
-  while (bit_is_set(ADCSRA, ADSC));
-  uint16_t result = ADC;
-  long vcc = 1125300L / result; 
-  return vcc;
-}
+
 
 
